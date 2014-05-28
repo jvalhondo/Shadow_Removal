94267104d6345ab2325a2be7d37661bfb0251511
Jorge Valhondo Rama <jrg.valhondo@gmail.com>
2014-05-28 15:47:58 +0200

    from scratch

--- Debug/src/shadow_removal.d
+++
@@ -1,6 +0,0 @@
-src/shadow_removal.d: ../src/shadow_removal.cpp \
- /usr/local/include/opencv/cv.h /usr/local/include/opencv/highgui.h
-
-/usr/local/include/opencv/cv.h:
-
-/usr/local/include/opencv/highgui.h:
